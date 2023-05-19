77722cec972920ff9c73107555dd6fc236fffc9e
Leader254 <gatheru.samuel@students.kyu.ac.ke>
2023-05-19 19:08:01 +0300

    refined code

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,54 @@
-/* eslint-disable no-unused-vars */
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [universities, setUniversities] = useState([]);
-  const [country, setCountry] = useState('');
+  const [region, setRegion] = useState("");
+  const [error, setError] = useState(null);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`http://universities.hipolabs.com/search?country=${country}`);
-      const data = await response.json();
-      setUniversities(data);
-    } catch (error) {
-      console.error('Error fetching university data:', error);
-    }
+  const handleSearch = () => {
+    setError(null);
   };
 
   useEffect(() => {
-    fetchData();
-  },[]);
+    if (region === "") {
+      return;
+    }
 
-  const searchCountry = (e) => {
-    setCountry(e.target.value);
-  };
+    fetch(`http://universities.hipolabs.com/search?country=${region}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUniversities(data);
+      })
+      .catch(() => {
+        setError("An error occurred while fetching universities.");
+      });
+  }, [region]);
 
   return (
-    <div className="container">
-      <div className="card-container">
-        <input placeholder='Search university...' onChange={searchCountry} type="search" />
-        <button onClick={fetchData}>Search</button>
+    <div>
+      <h1>Hello and welcome to the university portal!</h1>
+
+      <div className="search-bar">
+        <label htmlFor="region">Region:</label>
+        <input
+          type="text"
+          id="region"
+          value={region}
+          onChange={(e) => setRegion(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+        <button onClick={() => setRegion("")}>Clear</button>
       </div>
-      <div className="card-wrapper">
+
+      {error && <p>{error}</p>}
+
+      <div className="grid-container">
         {universities.map((university, index) => (
-          <div className="card" key={index}>
-            <p className="card-info">Name: {university.name}</p>
-            <p className="card-info">Website: {university.web_pages[0]}</p>
-            <p className="card-info">Country: {university.country}</p>
-            <hr />
+          <div className="card" key={university.web_pages[0] + index}>
+            <h3>{university.name}</h3>
+            <p>{university.country}</p>
+            <p>{university.web_pages[0]}</p>
           </div>
         ))}
       </div>
